f100e8afaaa4e842074dca8aa38ad5a86b74a611
minyo <005snail@gmail.com>
2019-01-25 19:21:48 +0900

    make dice in 3d and add rabbit face to front side

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -2,22 +2,6 @@ import React, { Component } from 'react';
 
 class Canvas extends Component {
 
-    /*
-    createCanvas = () => {
-        let canvas = []
-        let pixels = []
-
-        //create pixels
-        for (let i = 0; i < 400; i++) {
-            pixels.push(<div class="pixel"></div>)
-        }
-        //create canvas and add pixels
-        canvas.push(<div class="canvas">{pixels}</div>)
-
-        return canvas
-    }
-    */
-
     render() {
         return (
             <div className="icons">
